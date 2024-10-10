19224ea788e783883ff53ae98edd3c37c08d8600
dailykoder <112426800+dailykoder@users.noreply.github.com>
2024-10-09 21:24:21 -0700

    Updates

--- src/routes/work.lazy.tsx
+++
@@ -1,6 +0,0 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
-import Work from "@/pages/Work";
-
-export const Route = createLazyFileRoute("/work")({
-  component: () => <Work />,
-});
