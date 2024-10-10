d86dd69a8deebb60a7266add9fe11ac167af9785
dailykoder <112426800+dailykoder@users.noreply.github.com>
2024-10-10 15:48:25 -0700

    Updates

--- assets/about.lazy-BLAIJydE.js
+++
@@ -1,1 +0,0 @@
-import{j as t,c as o}from"./index-DRk7YBeV.js";const e=()=>t.jsx(t.Fragment,{children:"About"}),n=o("/about")({component:()=>t.jsx(e,{})});export{n as Route};
---
+++ assets/about.lazy-COY8eppv.js
@@ -0,0 +1,1 @@
+import{j as t,c as o}from"./index-BAsYjAv9.js";const e=()=>t.jsx(t.Fragment,{children:"About"}),n=o("/about")({component:()=>t.jsx(e,{})});export{n as Route};
