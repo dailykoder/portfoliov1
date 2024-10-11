12759e4d769753ab902e9937f955586b38257cec
dailykoder <112426800+dailykoder@users.noreply.github.com>
2024-10-11 11:56:42 -0700

    added window-installation page, changed routing so root route prefix is portfoliov1

--- src/components/nav/Sidebar.tsx
+++ src/components/nav/Sidebar.tsx
@@ -10,22 +10,24 @@ import { Link } from "@tanstack/react-router";
 const Sidebar = () => {
   return (
     <NavigationMenu>
-      <NavigationMenuList className="flex flex-col items-start items-start">
+      <NavigationMenuList className="flex flex-col items-start">
         <div className="flex pb-3">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-            />
-          </svg>
+          <Link to="/portfoliov1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+          </Link>
           <Link to="/portfoliov1" className="font-bold pl-1">
             Home
           </Link>
@@ -52,24 +54,38 @@ const Sidebar = () => {
             <NavigationMenuItem>
               <ul className="flex flex-col">
                 <NavigationMenuLink className="pt-1">
-                  <Link to="/portfoliov1/setdesign">Set Design</Link>
+                  <div>
+                    <Link to="/portfoliov1/setdesign">Set Design</Link>
+                  </div>
                 </NavigationMenuLink>
                 <NavigationMenuLink className="pt-1">
-                  <Link to="/portfoliov1/window-installation">
-                    Window Installation
-                  </Link>
+                  <div className="">
+                    <Link to="/portfoliov1/window-installation">
+                      Window Installation
+                    </Link>
+                  </div>
                 </NavigationMenuLink>
                 <NavigationMenuLink className="pt-1">
-                  Digital Rendering
+                  <div className="">
+                    <Link to="/portfoliov1/digital-rendering">
+                      Digital Rendering
+                    </Link>
+                  </div>
                 </NavigationMenuLink>
                 <NavigationMenuLink className="pt-1">
-                  Hand Rendering
+                  <div className="">
+                    <Link to="/portfoliov1/hand-rendering">Hand Rendering</Link>
+                  </div>
                 </NavigationMenuLink>
                 <NavigationMenuLink className="pt-1">
-                  Branding
+                  <div className="">
+                    <Link to="/portfoliov1/branding">Branding</Link>
+                  </div>
                 </NavigationMenuLink>
                 <NavigationMenuLink className="pt-1">
-                  Styling
+                  <div className="">
+                    <Link to="/portfoliov1/styling">Styling</Link>
+                  </div>
                 </NavigationMenuLink>
               </ul>
             </NavigationMenuItem>
