781c605f2e543114772faed755de74eec40fa6a8
dailykoder <112426800+dailykoder@users.noreply.github.com>
2024-10-13 19:27:12 -0700

    Updates

--- assets/anvil-mPNqAMsV.js
+++ assets/anvil-mPNqAMsV.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-b27DydpE.js";/**
+import{c as a}from"./index-Bs4SAqp6.js";/**
  * @license lucide-react v0.446.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/palette-D7GxXL6u.js
+++ assets/palette-D7GxXL6u.js
@@ -1,4 +1,4 @@
-import{c}from"./index-b27DydpE.js";/**
+import{c}from"./index-Bs4SAqp6.js";/**
  * @license lucide-react v0.446.0 - ISC
  *
  * This source code is licensed under the ISC license.
