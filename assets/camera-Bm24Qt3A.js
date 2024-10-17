838d17d362c704f46139fc3bc235547e00497e5c
dailykoder <112426800+dailykoder@users.noreply.github.com>
2024-10-17 15:35:16 -0700

    Updates

--- assets/camera-Bm24Qt3A.js
+++ assets/camera-Bm24Qt3A.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-DbHfsLY-.js";/**
+import{c as a}from"./index-CmkzRlC4.js";/**
  * @license lucide-react v0.446.0 - ISC
  *
  * This source code is licensed under the ISC license.
